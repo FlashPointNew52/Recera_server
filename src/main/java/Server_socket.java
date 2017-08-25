884e65af41d768adae57fdd742da748365ccb362
FlashPointNew52 <john.modenov@gmail.com>
2017-08-26 00:42:46 +1000

    rr4

--- src/main/java/Server_socket.java
+++ src/main/java/Server_socket.java
@@ -17,39 +17,32 @@ import javax.websocket.server.ServerEndpoint;
 @ServerEndpoint(value = "/")
 
 public class Server_socket {
-    //public HashSet<newAccept> clients = new HashSet();
-    private Map<String, Object> properties;
     private HashMap<String, Session> clients;
     private static final Logger LOGGER =
             Logger.getLogger(Server_socket.class.getName());
 
     @OnOpen
     public void onOpen(Session session, EndpointConfig config) throws IOException {
-        session.getBasicRemote().sendText("onOpen");
-        properties = config.getUserProperties();
         clients.put(session.getId(), session);
-        LOGGER.log(Level.INFO, "New connection with client: {0}", properties
-                );
+        LOGGER.log(Level.INFO, "New connection with client: {0}");
     }
 
     @OnMessage
     public String onMessage(String message, Session session) throws IOException {
-        LOGGER.log(Level.INFO, "New message from Client [{0}]: {1}",
-                new Object[] {session.getOpenSessions(), message});
+        LOGGER.log(Level.INFO, "New message from Client [{0}]: {1}", new Object[] {session.getOpenSessions(), message});
         for (Map.Entry<String, Session> entry : clients.entrySet()) {
             entry.getValue().getBasicRemote().sendText(message);
         }
-       return "";
+        return "";
     }
 
     @OnClose
     public void onClose(Session session) {
-        LOGGER.log(Level.INFO, "Close connection for client: {0}",
-                session.getId());
+        LOGGER.log(Level.INFO, "Close connection for client: {0}", session.getId());
     }
 
     @OnError
     public void onError(Throwable exception, Session session) {
-        LOGGER.log(Level.INFO, "Error for client: {0}", session.getId());
+        LOGGER.log(Level.INFO, "Error for client: {0}", exception.getMessage());
     }
 }
